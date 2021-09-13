f6e12ddd2f034b9d7764c1edc8a7665b7acd96a7
carlincb <carlin.colleen@gmail.com>
2021-09-12 19:17:24 -0600

    Added comments to js file.

--- script.js
+++ script.js
@@ -10,25 +10,28 @@ var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var minLength = 8;
 var maxLength = 128;
 
-// passwordInput = numbers.concat(specialCharacters, upperCase, lowerCase);
-
-// Write password to the #password input
 function generatePassword() {
-  var newPassword = [];
-  var potentialCharacters = [];
-
+  
+  // Created two variables.
+  var newPassword = [];// Created "newPassword" to store all values for the new password created by the function.
+  var potentialCharacters = [];// Created "potentialCharacters" to store all potential characters the user could choose.
+  
+  //Variable "passwordLength" included to control the character length of the password and coupled with the prompt command so user can input desired length.  
   var passwordLength = prompt("Please input a number from " + minLength + " to " + maxLength + " to choose the number of characters in your password.");
-
+  
+  //Created "if" statement to exclude passwords that are too long, short, not numbers, or nothing is entered. 
   if(passwordLength < 8 || passwordLength > 128 || passwordLength==="" || passwordLength===NaN){
     alert("Please select a number from 8 to 128 to continue.");
     return;
   };
-
+  
+  // Created variables to house booleans for confirm statements to reference.
   var useNumbers=confirm("Would you like to use numbers in your password? If so, click 'OK.'");
   var useSpecialCharacters=confirm("Would you like to use special characters in your password? If so, click 'OK.'");
   var useLowerCase=confirm("Would you like to use lowercase letters in your password? If so, click 'OK.'");
   var useUpperCase=confirm("Would you like to use uppercase letters in your password? If so, click 'OK.'");
-
+  
+  // Created "if" statement to ensure one option is selected for potential password.
   if(!useNumbers && !useSpecialCharacters && !useUpperCase && !useLowerCase) {
     alert("Please select at least one option.");
     return;
@@ -55,7 +58,7 @@ function generatePassword() {
   for (let i = 0; i < remainingLength; i++) {
     newPassword.push(getRandomCharacter(potentialCharacters))
   };
-  return newPassword.join('');
+  return newPassword.join('');//Used join tag to convert newPassword from an array to a string.
 };
 
 function getRandomCharacter(array) {
@@ -63,6 +66,7 @@ function getRandomCharacter(array) {
   return randomCharacter;
 };
 
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
